587fe617646f8471c317d7df445725d05fcb0e4e
youssef4140 <youssef.mostafa4140@gmail.com>
2024-08-28 15:29:06 +0300

    dashboard init

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,6 @@
+<template>
+
+<h1>dashboard</h1>
+
+
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import loginPage from '@/components/Login/LoginPage.vue'
+</script>
+
+<template>
+  <main>
+    <loginPage />
+  </main>
+</template>
+<style scoped lang="scss">
+h1{
+  }
+</style>
+
