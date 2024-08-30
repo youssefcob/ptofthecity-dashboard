ef1a2373e23c44d9d2977d76409adce2cec2e55e
youssef4140 <youssef.mostafa4140@gmail.com>
2024-08-30 18:26:58 +0300

    navbar and modal

---
+++ src/components/Dashboard/navbar/Navbar.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const activeButton = ref('');
+
+const handleClick = (buttonName: string) => {
+    activeButton.value = buttonName;
+};
+
+onMounted(() => {
+    let windowLocation = window.location.pathname.split('/').pop();
+    if(windowLocation)handleClick(windowLocation);
+});
+</script>
+
+<template>
+    <nav>
+        <div class="btns-wrapper">
+            <button class="nav-btn" :class="{ active: activeButton === '' }" @click="handleClick('')">
+                Dashboard
+            </button>
+            <button class="nav-btn" :class="{ active: activeButton === 'reservations' }" @click="handleClick('reservations')">
+                Reservations
+            </button>
+            <button class="nav-btn" :class="{ active: activeButton === 'eligibility' }" @click="handleClick('eligibility')">
+                Eligibility
+            </button>
+            <button class="nav-btn" :class="{ active: activeButton === 'careers' }" @click="handleClick('careers')">
+                Careers
+            </button>
+            <button class="nav-btn" :class="{ active: activeButton === 'messages' }" @click="handleClick('messages')">
+                Messages
+            </button>
+            <router-link to="/dashboard/clinics" class="nav-btn" :class="{ active: activeButton === 'clinics' }" @click="handleClick('clinics')">
+                Clinics
+            </router-link>
+            <router-link to="/dashboard/services" class="nav-btn" :class="{ active: activeButton === 'services' }" @click="handleClick('services')">
+                Services
+            </router-link>
+            <router-link to="/dashboard/insurances" class="nav-btn" :class="{ active: activeButton === 'insurances' }" @click="handleClick('insurances')">
+                Insurances
+            </router-link>
+            <button class="nav-btn" :class="{ active: activeButton === 'whoWeAre' }" @click="handleClick('whoWeAre')">
+                Who We Are
+            </button>
+
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+
+    .btns-wrapper {
+        display: flex;
+        flex-direction: column;
+        height:100vh;
+        width:15%;
+        min-width: 15rem;
+        border: 1px solid black;
+        border-top:none;
+        // border-radius: 10px;
+        overflow: hidden;
+
+        .nav-btn {
+            background-color: transparent;
+            padding: 1rem 1.5rem;
+            border: none;
+            //   border-radius: 10px;
+            cursor: pointer;
+            transition: all 0.3s;
+            text-align: start;
+
+            color:black;
+            font-size: 1.3rem;
+            font-family: $montserrat;
+
+            &.active {
+                background-color: $blue;
+            }
+
+            &:hover:not(.active) {
+                background-color: $darkgrey;
+            }
+        }
+    }
+}
+</style>
---
+++ src/components/sharedComponents/Modal.vue
@@ -0,0 +1,82 @@
+<template>
+
+    <div>
+        <!-- <button ref="open" @click="openModal">Open</button> -->
+        <div class="modal-container">
+            <dialog ref="modal">
+                <div ref="container">
+                    <slot></slot>
+                </div>
+            </dialog>
+        </div>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const modal: Ref<HTMLDialogElement | null> = ref(null);
+
+// const open: Ref<HTMLButtonElement | null> = ref(null);
+let modalState = ref(false);
+const container: Ref<HTMLElement | null> = ref(null);
+const openModal = () => {
+    if (modal.value) modal.value.showModal();
+    setTimeout(() => {
+        modalState.value = true;
+
+    }, 100);
+
+}
+const closeModal = () => {
+    if (modal.value) modal.value.close();
+    modalState.value = false;
+
+}
+
+document.addEventListener("click", (e) => {
+
+    if (modalState.value == true && !container.value?.contains(e.target as Node)) {
+        modalState.value = false;
+        closeModal();
+    }
+});
+defineExpose({
+    openModal,
+    closeModal
+});
+
+</script>
+
+<style scoped lang="scss">
+dialog {
+
+    background-color: rgba(44, 50, 51, 0.40);
+    position: fixed;
+    border: none;
+    top: 10%;
+    left: 15%;
+    user-select: none;
+
+    margin:auto;
+    @media screen and (max-width: 800px) {
+        top: 5%;
+        left: 5%;
+
+    }
+
+    // z-index: 4;
+    &::backdrop {
+        background-color: rgba(44, 50, 51, 0.80);
+        user-select: none;
+
+    }
+
+    .container {
+        overflow: hidden;
+        user-select: none;
+
+    }
+}
+</style>
