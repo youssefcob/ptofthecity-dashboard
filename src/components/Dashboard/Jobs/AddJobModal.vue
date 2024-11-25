df88cdbf48774c98cb65cdf9b6bb6e0b48ea63cc
youssefcob <164803162+youssefcob@users.noreply.github.com>
2024-11-25 05:37:51 +0200

    added loading to jobs

--- src/components/Dashboard/Jobs/AddJobModal.vue
+++ src/components/Dashboard/Jobs/AddJobModal.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
+import Btn from '@/components/sharedComponents/btn.vue';
 import InputField from '@/components/sharedComponents/InputField.vue';
 import RadioInputField from '@/components/sharedComponents/RadioInputField.vue';
 import Http from '@/mixins/Http';
+import { ref } from 'vue';
 import { reactive } from 'vue';
 
 const form = reactive({
     title: '',
     description: '',
-    isAvailable: false,
+    isAvailable: 0,
 });
 
 const assignToForm = (value: string) => {
-    form.isAvailable = (value == 'Available')
+
+    form.isAvailable = (value === 'Available')? 1:0;
 }
+const loading = ref(false);
 const submit = async () => {
+
+    loading.value = true;
     const res = await Http.post('career/jobs', form, {
         'Authorization': `Bearer ${localStorage.getItem('token')}`
     });
+    loading.value = false;
+
+    if (res.status === 401) window.location.href = '/login';
+    if (res.status === 201) {
+        window.location.reload();
+    } else {
+        alert(res.data.error);
+    }
     console.log(res);
     // console.log(form);
 }
@@ -33,7 +47,7 @@ const submit = async () => {
         <InputField @input="form.description = $event" height="20rem" label="Job Description" type="text" placeHolder="Enter Job Description" />
 
 
-        <button class="btn" @click="submit">Submit</button>
+        <Btn  @click="submit" :loading="loading"> Submit</Btn>
     </div>
 </template>
 
